7159bce1de910428d3ffc52dfae132646514b12b
Verbum <46956900+georgy4932@users.noreply.github.com>
2026-05-19 00:26:15 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-const root = document.getElementById('root')
-
-if (!root) {
-  throw new Error('Root element not found')
-}
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ng">
       <App />
     </BrowserRouter>
   </React.StrictMode>
